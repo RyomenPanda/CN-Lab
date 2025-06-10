2db0966e44481204f67bc8448b02b8f898640f61
Panda <91757462+RyomenPanda@users.noreply.github.com>
2025-06-11 01:19:20 +0530

    Create FibonacciSeries.py

---
+++ Python/FibonacciSeries.py
@@ -0,0 +1,16 @@
+def fibonacci(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    else:
+        seq = fibonacci(n - 1)
+        seq.append(seq[-1] + seq[-2])
+        return seq
+
+# Input
+n = int(input("Enter number of terms: "))
+print("Fibonacci series:")
+print(fibonacci(n))
